0fd6173a16f0e0bb0ad103925e83b8b43d49b8e8
Oscar Alvarado <alumnos.soft@gmail.com>
2026-08-08 11:01:00 -0600

    feat: crear API inicial con Express y endpoint health

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    mensaje: "API DevOps funcionando correctamente"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    estado: "OK",
+    mensaje: "API operativa"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
